2f1fc62a51009ed4885c6020670bcf68a661124f
Sergio Gimeno <sergio.gimeno-saldana@est.fib.upc.edu>
2012-07-24 15:56:11 +0200

    playing on landing page

--- landing/views.py
+++ landing/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
-import smtplib, random, string, md5, os
+import smtplib, random, string, md5, os, datetime
 from django.contrib.auth.models import User
 
 def welcome(request):
-        name = 'pepe'
-        return render_to_response('home.html', {'name': name})
+    name = 'pepe'
+    now = datetime.datetime.now()
+    return render_to_response('home.html', {'name': name, 'time': now})
