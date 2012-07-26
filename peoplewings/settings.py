a1759ee5f96b5ffc72c27b4c6badb6ea52c5b6da
Sergio Gimeno <sergio.gimeno-saldana@est.fib.upc.edu>
2012-07-26 16:39:28 +0200

    Fixed STATICFILES_DIR path

--- peoplewings/settings.py
+++ peoplewings/settings.py
@@ -74,7 +74,7 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     "/Users/sergio/Workspace/peoplewings/static/",
-    "/Users/sergio/Workspace/peoplewings/static/css",
+
 )
 
 # List of finder classes that know how to find static files in
