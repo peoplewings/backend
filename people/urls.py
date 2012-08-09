7ad2821e284557290a5ce65b16ca8e935d6212cd
Sergio Gimeno <sergio.gimeno-saldana@est.fib.upc.edu>
2012-08-09 10:50:05 +0200

    Changed function names

--- people/urls.py
+++ people/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('people.views',
 	url(r'^profile/$', 'view_profile'),
-    url(r'^profile/edit/$', 'enter_edit_rofile'),
+    url(r'^profile/edit/$', 'enter_edit_profile'),
     url(r'^profile/edit/basic$', 'enter_edit_basic_information'),
+    url(r'^profile/formset$', 'formset_play'),
     url(r'^profile/edit/completed/$', 'edit_profile'),
     url(r'^basic/edit/completed/$', 'edit_basic_information'),
     url(r'^account/$', 'view_account_settings'),
