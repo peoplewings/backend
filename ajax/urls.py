bcbb8bc364f496c6a6c47101c761bcb320df07cc
Sergio Gimeno <sergio.gimeno-saldana@est.fib.upc.edu>
2012-08-28 16:50:00 +0200

    Added upload_image handler

--- ajax/urls.py
+++ ajax/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 
 
 urlpatterns = patterns('ajax.views',
-	url(r'^search/university/$', 'searchUniversity'),
+	url(r'^search/university/$', 'search_university'),
+	url(r'^upload/image/$', 'upload_image'),
 )
--- ajax/views.py
+++ ajax/views.py
@@ -1,10 +1,13 @@
 # Create your views here.
 from people.models import University
 import json
-from django.http import HttpResponse
+import datetime
+from peoplewings.settings import MEDIA_ROOT
+from django.http import HttpResponse, HttpResponseRedirect
 from django.db.models import Q
+from django.contrib.auth.decorators import login_required
 
-def searchUniversity(request):
+def search_university(request):
 	query=request.GET.get('q')
 	response_data = {}
 	#response_data['code'] = 1
@@ -29,3 +32,23 @@ def searchUniversity(request):
 	response_data['code'] = HttpResponse.status_code
 	    
 	return HttpResponse(json.dumps(response_data), mimetype="application/json")
+
+@login_required
+def upload_image(request):
+    response = {'success':False}
+    if request.method == 'POST':
+        #, path=str(request.user.pk) + "/"
+        save_file(request.FILES['fileToUpload'])
+    js = json.dumps(response)
+    return HttpResponse(js, mimetype='application/json')
+
+def save_file(file, path=''):
+    ''' Little helper to save a file
+    '''
+    #filename = file._get_name()
+    filename = "avatar.jpg"
+    fd = open('%s/%s' % (MEDIA_ROOT, str(path) + str(filename)), 'wb')
+    for chunk in file.chunks():
+        fd.write(chunk)
+    fd.close()
+
