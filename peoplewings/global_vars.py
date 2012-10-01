a873b83073410a9724d3940cecbe9e3fe9436ef1
ezequiel_anokian <eze_anokian@hotmail.com>
2012-10-01 15:24:44 +0200

    working on profile

--- peoplewings/global_vars.py
+++ peoplewings/global_vars.py
@@ -1,1 +1,35 @@
 max_500_char = 500
+max_long_len = 200
+
+PW_STATE_CHOICES = (
+    ('Y', 'Yes'),
+    ('N', 'No'),
+    ('M', 'Maybe'),
+    ('T', 'Traveling'),
+    ('C', 'Coffee or Drink'),
+    ('W', 'By wing'),
+)
+
+BIRTHDAY_CHOICES = (
+    ('F', 'My full birthday'),
+    ('P', 'Only month & day'),
+    ('N', 'Don\'t show'),
+)
+
+GENDER_CHOICES = (
+    ('M', 'Male'),
+    ('F', 'Female'),
+)
+
+CIVIL_STATE_CHOICES = (
+    ('','Empty'),
+    ('SI', 'Single'),
+    ('EN', 'Engaged'),
+    ('MA', 'Married'),
+    ('WI', 'Widowed'),
+    ('IR', 'In a relationship'),
+    ('IO', 'In an open relationship'),
+    ('IC', 'It\'s complicated'),
+    ('DI', 'Divorced'),
+    ('SE', 'Separated'),
+)
