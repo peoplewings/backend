dfec1430c9e4dd1b82cf7ba7a523da8753a2c9f9
ezequiel_anokian <eze_anokian@hotmail.com>
2012-10-24 16:13:35 +0200

    changed model for City, Region & Country

--- peoplewings/apps/locations/api.py
+++ peoplewings/apps/locations/api.py
@@ -1,33 +1,37 @@
 from peoplewings.apps.locations.models import Country, Region, City
-
-
-from tastypie.resources import ModelResource
 from peoplewings.apps.ajax.utils import CamelCaseJSONSerializer
 from peoplewings.apps.registration.authentication import ApiTokenAuthentication
+
+from tastypie.resources import ModelResource
+from tastypie import fields
 
 class CountryResource(ModelResource):
     class Meta:
         object_class = Country
         queryset = Country.objects.all()
         allowed_methods = []
-        #include_resource_uri = False
+        include_resource_uri = False
         serializer = CamelCaseJSONSerializer(formats=['json'])
         authentication = ApiTokenAuthentication()
         #authorization = Authorization()
         #always_return_data = True
+        excludes = ['id']
 
 class RegionResource(ModelResource):
+    #country = fields.ToOneField(CountryResource, 'country', full=True, null=True)
     class Meta:
         object_class = Region
         queryset = Region.objects.all()
         allowed_methods = []
-        #include_resource_uri = False
+        include_resource_uri = False
         serializer = CamelCaseJSONSerializer(formats=['json'])
         authentication = ApiTokenAuthentication()
         #authorization = Authorization()
         #always_return_data = True
+        excludes = ['id']
 
 class CityResource(ModelResource):
+    #region = fields.ToOneField(RegionResource, 'region', full=True, null=True)
     class Meta:
         object_class = City
         queryset = City.objects.all()
@@ -41,3 +45,4 @@ class CityResource(ModelResource):
         filtering = {
             "name": ['exact'],
         }
+
