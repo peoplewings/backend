7902f64881aee3ec36185c364e63c447309d75e5
Joan Roca <fr33d4n@gmail.com>
2012-12-27 16:54:06 +0100

    lol

--- peoplewings/apps/notifications/test_notifications.py
+++
@@ -1,7 +0,0 @@
-from domain import NotificationsList
-from api import NotificationsListResource
-from nose.tools import assert_equal
-from nose.tools import assert_not_equal
-from nose.tools import assert_raises
-from nose.tools import raises
-
