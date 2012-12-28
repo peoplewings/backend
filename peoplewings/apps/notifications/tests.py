6b67e170f191ec8a3c765c82e519012875eafe67
ezequiel_anokian <eze_anokian@hotmail.com>
2012-12-28 12:57:07 +0100

    improved tests using fixtures and in-memory database

--- peoplewings/apps/notifications/tests.py
+++ peoplewings/apps/notifications/tests.py
@@ -15,8 +15,11 @@ from django.utils.timezone import utc
 import json
  
 class PaginationTest(TestCase):
- 
+	fixtures = ['locations.json', 'user.json', 'people.json', 'wings.json']
+
+	
 	def setUp(self):
+		'''
 		self.hadeer = User.objects.create_user(username = 'a@a.com', password = '123456')
 		self.ph = UserProfile.objects.create(user = self.hadeer, birthday="1987-03-04")
 
@@ -34,30 +37,42 @@ class PaginationTest(TestCase):
 		self.n7 = Messages.objects.create(sender = self.ph, receiver = self.pj, reference="2")
 		self.n8 = Messages.objects.create(sender = self.pj, receiver = self.ph, reference="3")
 
+		'''
+		self.eze = UserProfile.objects.get(user=User.objects.get(email='ezequiel@peoplewings.com'))
+		self.joan = UserProfile.objects.get(user=User.objects.get(email='joan@peoplewings.com'))
+
+		for i in range(5):
+			Messages.objects.create(sender = self.eze, receiver = self.joan, reference=str(i))
+		
+	
  
 	def test_models(self):
-		self.assertEqual(Notifications.objects.filter(receiver=self.pj).count(), Notifications.objects.filter(sender=self.ph).count())
-		self.assertEqual(Notifications.objects.filter(sender=self.ph).count(), 5)
 
-		self.assertEqual(Notifications.objects.filter(sender=self.pj).count(), Notifications.objects.filter(receiver=self.ph).count())
-		self.assertEqual(Notifications.objects.filter(receiver=self.ph).count(), 3)
+		self.eze = UserProfile.objects.get(user=User.objects.get(email='ezequiel@peoplewings.com'))
+		self.joan = UserProfile.objects.get(user=User.objects.get(email='joan@peoplewings.com'))
+
+		self.assertEqual(Notifications.objects.filter(receiver=self.eze).count(), Notifications.objects.filter(sender=self.joan).count())
+		self.assertEqual(Notifications.objects.filter(receiver=self.eze).count(), 0)
+
+		self.assertEqual(Notifications.objects.filter(sender=self.eze).count(), Notifications.objects.filter(receiver=self.joan).count())
+		self.assertEqual(Notifications.objects.filter(sender=self.eze).count(), 5)
 
 	def test_not_logged_in(self):
 		c = Client()
-		response = c.get('/api/v1/notificationslist?kind=reqinv', content_type='application/json')
+		response = c.get('/api/v1/notificationslist', content_type='application/json')
 		self.assertEqual(response.status_code, 401)
  
 	def test_logged_in(self):
 		c = Client()
 		#c.login(username='Hadeer', password='123456')
-		response = c.post('/api/v1/auth', json.dumps({ 'username' : 'a@a.com', 'password' : '123456'}), content_type='application/json')
+		response = c.post('/api/v1/auth', json.dumps({ 'username' : 'ezequiel@peoplewings.com', 'password' : 'asdf'}), content_type='application/json')
 		token = json.loads(response.content)['data']['xAuthToken']		
-		response = c.get('/api/v1/notificationslist?kind=reqinv', HTTP_X_AUTH_TOKEN=token, content_type='application/json')
+		response = c.get('/api/v1/notificationslist', HTTP_X_AUTH_TOKEN=token, content_type='application/json')
 		self.assertEqual(response.status_code, 200)
 
 	def test_pagination(self):
 		c = Client()		
-		response = c.post('/api/v1/auth', json.dumps({ 'username' : 'a@a.com', 'password' : '123456'}), content_type='application/json')
+		response = c.post('/api/v1/auth', json.dumps({ 'username' : 'ezequiel@peoplewings.com', 'password' : 'asdf'}), content_type='application/json')
 		token = json.loads(response.content)['data']['xAuthToken']		
 		
 		#check the first page exists
@@ -65,7 +80,7 @@ class PaginationTest(TestCase):
 		self.assertEqual(json.loads(r1.content)['code'], 200)
 		self.assertEqual(json.loads(r1.content)['msg'], "OK")
 		self.assertEqual(json.loads(r1.content)['status'], True)
-		self.assertEqual(json.loads(r1.content)['data']['count'], 3)
+		self.assertEqual(json.loads(r1.content)['data']['count'], 5)
 		self.assertEqual(json.loads(r1.content)['data']['endResult'], 2)
 		self.assertEqual(json.loads(r1.content)['data']['startResult'], 1)
 		self.assertEqual(len(json.loads(r1.content)['data']['items']), 2)
@@ -76,25 +91,25 @@ class PaginationTest(TestCase):
 		self.assertEqual(json.loads(r2.content)['code'], 200)
 		self.assertEqual(json.loads(r2.content)['msg'], "OK")
 		self.assertEqual(json.loads(r2.content)['status'], True)
-		self.assertEqual(json.loads(r2.content)['data']['count'], 3)
+		self.assertEqual(json.loads(r2.content)['data']['count'], 5)
 		self.assertEqual(json.loads(r2.content)['data']['endResult'], 2)
 		self.assertEqual(json.loads(r2.content)['data']['startResult'], 1)
 		self.assertEqual(len(json.loads(r2.content)['data']['items']), 2)
 		l2 = json.loads(r2.content)['data']['items']
 		self.assertEqual(l1, l2)
 
-		#check that not providing the page parameter has the same effect as calling with page=1
-		r4 = c.get('/api/v1/notificationslist?page=2', HTTP_X_AUTH_TOKEN=token, content_type='application/json')
+		#check last page
+		r4 = c.get('/api/v1/notificationslist?page=3', HTTP_X_AUTH_TOKEN=token, content_type='application/json')
 		self.assertEqual(json.loads(r4.content)['code'], 200)
 		self.assertEqual(json.loads(r4.content)['msg'], "OK")
 		self.assertEqual(json.loads(r4.content)['status'], True)
-		self.assertEqual(json.loads(r4.content)['data']['count'], 3)
-		self.assertEqual(json.loads(r4.content)['data']['endResult'], 3)
-		self.assertEqual(json.loads(r4.content)['data']['startResult'], 3)
+		self.assertEqual(json.loads(r4.content)['data']['count'], 5)
+		self.assertEqual(json.loads(r4.content)['data']['endResult'], 5)
+		self.assertEqual(json.loads(r4.content)['data']['startResult'], 5)
 		self.assertEqual(len(json.loads(r4.content)['data']['items']), 1)
 
 		# checking unexisting pages...
-		r3 = c.get('/api/v1/notificationslist?page=3', HTTP_X_AUTH_TOKEN=token, content_type='application/json')
+		r3 = c.get('/api/v1/notificationslist?page=4', HTTP_X_AUTH_TOKEN=token, content_type='application/json')
 		self.assertEqual(json.loads(r3.content)['code'], 413)
 		self.assertEqual(json.loads(r3.content)['msg'], "Sorry, no results on that page.")
 		self.assertEqual(json.loads(r3.content)['status'], False)
