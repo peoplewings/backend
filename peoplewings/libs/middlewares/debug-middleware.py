e2dcec826ba9cd1e9f3545e5671540cb21737ebe
Joan Roca <fr33d4n@gmail.com>
2013-02-13 16:22:25 +0100

    Errors done, and middleware added

--- peoplewings/libs/middlewares/debug-middleware.py
+++ peoplewings/libs/middlewares/debug-middleware.py
@@ -1,11 +1,52 @@
+from django.conf import settings
+import logging
+import json
+import datetime
+
 class DebugMiddleware(object):
 
-	def process_request(self, request):
-		print 'DebugMiddleware\n'
-		print request
-		print '---------------------------\n'
+	def process_request(self, request):	
 		return None
 
-	def process_response(self, request, response):
+	def log_fine(self, request, response):
+		today = datetime.datetime.today()
+		log_date = '%s-%s-%s' % (today.day, today.month, today.year)
+		logging.basicConfig(filename='log/%s.log' % log_date, format='%(asctime)s  %(levelname)s  %(message)s', level=logging.INFO)
+		token = getattr(request.META, 'HTTP_X_AUTH_TOKEN', 'NO_TOKEN')
+		content = json.loads(response.content)
+		post_params = request.raw_post_data
+		get_params = json.dumps(request.GET)
+		
+		if content.has_key('status'):
+			if content['status'] == False:
+				if content.has_key('errors'):
+					for i in content['errors']:
+						logging.error('%s %s %s %s %s %s' % (request.META['REQUEST_METHOD'], request.META['PATH_INFO'], token, i["type"], get_params, post_params))
+				else:
+					logging.critical('%s %s %s %s' % (request.META['REQUEST_METHOD'], request.META['PATH_INFO'], token, 'NO_ERRORS_FIELD'))
+			else:
+				logging.info('%s %s %s %s' % (request.META['REQUEST_METHOD'], request.META['PATH_INFO'], token, 'OK'))
+		else:
+			logging.critical('%s %s %s %s' % (request.META['REQUEST_METHOD'], request.META['PATH_INFO'], token, 'NO_STATUS_FIELD'))
+
+	def process_response(self, request, response):		
+		debug = getattr(settings, 'DEBUG', False)		
+		if debug:
+			#Debug == True. We will only log bad requests
+			self.log_fine(request, response)
+		else:
+			#Debug != False. We log AND take out the content of the response
+			self.log_fine(request, response)
+			content = json.loads(response.content)
+			if content.has_key('status'):
+				if content['status'] == False:
+					errors = []
+					errors_allowed = ["EMAIL_IN_USE", "AUTH_REQUIRED", "USED_KEY", "EXPIRED_KEY", "INVALID_USER_OR_PASS", "INACTIVE_USER", "AUTH_REQUIRED"]
+					if content.has_key('errors'):
+						for i in content['errors']:
+							if i['type'] in errors_allowed:
+								errors.append(i)
+					response.content = json.dumps({"status":False, "errors": errors})
+
 		return response
 
--- peoplewings/settings/testing.py
+++ peoplewings/settings/testing.py
@@ -68,35 +68,6 @@ EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'emailconfirm@peoplewings.com'
 SERVER_EMAIL = 'emailconfirm@peoplewings.com'
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
-
 #IMG
 AWS_ACCESS_KEY_ID = "AKIAI5TSJI7DYXGRQDYA"
 AWS_SECRET_ACCESS_KEY = "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"
