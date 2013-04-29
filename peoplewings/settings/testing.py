4afc0bf7b2c590af3f4318318ffb2f4c4dccf530
Joan Roca <fr33d4n@gmail.com>
2013-04-29 12:58:10 +0200

    Requirements

--- peoplewings/settings/testing.py
+++ peoplewings/settings/testing.py
@@ -15,7 +15,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'd6dvu92h1qihiq',        # Or path to database file if using sqlite3.
         'USER': 'tmfhgcdtkewoem',        # Not used with sqlite3.
-        'PASSWORD': 'F60gU7pnTuI5GkqdQO',           # Not used with sqlite3.
+        'PASSWORD': 'F60gU7pnTuI5GkqdQO-e53Gq_-',           # Not used with sqlite3.
         'HOST': 'ec2-54-247-64-184.eu-west-1.compute.amazonaws.com', # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
