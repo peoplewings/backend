7979e316cb162462126acb93525183441d0d0487
fr33d4n <fr33d4n@gmail.com>
2013-06-14 13:24:22 +0200

    Improved the DBControl

--- peoplewings/apps/dbcontrol/management/commands/apply_changes.py
+++ peoplewings/apps/dbcontrol/management/commands/apply_changes.py
@@ -1,7 +1,30 @@
 from django.core.management.base import BaseCommand, CommandError
-from dbcontrol.views import apply_changes
+from dbcontrol.views import apply_changes, list_changes, one_change
+from optparse import make_option
+
 class Command(BaseCommand):
 
+	help= 'Applies certan or all DB scripts in order'
+	option_list = BaseCommand.option_list + (
+        make_option('--list',
+            action='store_true',
+            dest='list',
+            default=False,
+            help='List all available scripts and see if we have applied them'),
+        ) + (
+        make_option('--one',
+            action='store_true',
+            dest='one',
+            default=False,
+            help='Applies the script taged with a certain number'),
+        )
+
 	def handle(self, *args, **options):
-		apply_changes()
+		if options['list']:
+			list_changes()
+		elif options['one']:
+			for arg in args:
+				one_change(arg)
+		else:
+			apply_changes()
 		self.stdout.write("Changes to the DB applyed succesfully\n")
--- peoplewings/apps/dbcontrol/views.py
+++ peoplewings/apps/dbcontrol/views.py
@@ -16,9 +16,11 @@ def get_script_list():
 	path = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'scripts'))
 	script_list = []
 	for root,dirs,files in os.walk(path):
-	    for f in files:
-	        if f.endswith(".py"):
-	            script_list.append({"name": f, "path": os.path.join(root,f), "ordering": f.split('-')[1]})
+		for f in files:
+			if f.endswith(".py"):
+				ordering = f.split('-')[1]
+				ordering = ordering.split('.')[0]
+				script_list.append({"name": f, "path": os.path.join(root,f), "ordering": ordering})
 
 	return script_list
 
@@ -34,6 +36,36 @@ def apply_individual_script(scr, name):
 		script = imp.load_source('*', scr)
 		script.main()
 		DbControl.objects.create(script_path = name, applied=True)
+
+def list_changes():
+	scr_list = get_script_list()
+	scr_list.sort(key=itemgetter('ordering'), reverse=False)
+	for src in scr_list:
+		print_individual_script(src['path'], src['name'])
+
+def print_individual_script(scr, name):
+	to_print = name
+	try:
+		current = DbControl.objects.get(script_path= name)
+		if current.applied is False:
+			to_print = to_print + ' ...... PENDING'
+		else:
+			to_print = to_print + ' ...... APPLIED'
+	except:
+		to_print = to_print + ' ...... PENDING'
+	print to_print
+
+def one_change(number):
+	scr_list = get_script_list()
+	#import pdb; pdb.set_trace()
+	#[{name=file, path=full path, ordering=order}]
+	if number in [str(i['ordering']) for i in scr_list]:
+		path, name = [(i['path'], i['name']) for i in scr_list if i['ordering'] == number][0]
+		#print path + ' ' + name 
+		apply_individual_script(path, name)
+		print 'Applied script n %s' % number
+	else:
+		print 'Could not find the selected script'
 
 
 
