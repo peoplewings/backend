f95fa850bcd680b6922930593158e73152c5f3f9
fr33d4n <fr33d4n@gmail.com>
2013-06-18 16:22:19 +0200

    Fixed feedback admin

--- peoplewings/apps/notifications/middleware.py
+++ peoplewings/apps/notifications/middleware.py
@@ -10,8 +10,10 @@ class Notification(object):
 	def process_request(self, request):
 		return None
 
-	def process_response(self, request, response):		
-		if request.user and not isinstance(request.user, AnonymousUser):
+	def process_response(self, request, response):	
+		#import pdb; pdb.set_trace()
+		compiled_regexp = re.compile("/admin/")
+		if request.user and not isinstance(request.user, AnonymousUser) and not compiled_regexp.match(str(request.path_info)):
 			if response.status_code == 200:		
 				resp = json.loads(response.content)
 				if not resp.has_key('updates'):
