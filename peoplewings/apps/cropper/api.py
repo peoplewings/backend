f20ef2bbba38bccbef2303a28cc1ffcdc50bb4e6
fr33d4n <fr33d4n@gmail.com>
2013-06-20 15:36:54 +0200

    Fixed https with amazon photos

--- peoplewings/apps/cropper/api.py
+++ peoplewings/apps/cropper/api.py
@@ -368,9 +368,10 @@ class CropbigResource(ModelResource):
 		POST= json.loads(encoded)
 		url = ""
 		try:
+			#string.replace(s, old, new[, maxreplace])
 			url = POST["results"]["images"][0]['s3_url']
+			url = str.replace(url, 'http', '', 1)
 			img_id = POST["results"]["images"][0]['image_identifier']
-
 			user_id = img_id.split("-")[0]
 			prof = UserProfile.objects.get(user__pk = int(user_id))
 			prof.avatar = url
@@ -468,6 +469,7 @@ class CropsmallResource(ModelResource):
 
 			user_id = img_id.split("-")[0]
 			prof = UserProfile.objects.get(user__pk = int(user_id))
+			url = str.replace(url, 'http', '', 1)
 			prof.thumb_avatar = url
 			prof.avatar_updated=True
 			prof.save()
